f5d5379cc485dadf71efd3e69cc494e54851aecd
Mohithash <shahjespuri@gmail.com>
2026-09-05 11:31:32 +0000

    touch: Follow the vendor.bestrom.touch rename
    The interface package was renamed in hardware/voltage/interfaces.
    peridot does not build the Pixel touch HAL, but Soong still parses
    the module, so it has to follow.
    
    Signed-off-by: Mohithash <shahjespuri@gmail.com>

--- touch/GloveMode.h
+++ touch/GloveMode.h
@@ -5,11 +5,11 @@
 
 #pragma once
 
-#include <aidl/vendor/lineage/touch/BnGloveMode.h>
+#include <aidl/vendor/bestrom/touch/BnGloveMode.h>
 
 namespace aidl {
 namespace vendor {
-namespace lineage {
+namespace bestrom {
 namespace touch {
 
 class GloveMode : public BnGloveMode {
@@ -19,6 +19,6 @@ class GloveMode : public BnGloveMode {
 };
 
 }  // namespace touch
-}  // namespace lineage
+}  // namespace bestrom
 }  // namespace vendor
 }  // namespace aidl
